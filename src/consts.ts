7279140b5c1c8e7ff27d0cd4847de5a06701489c
mio¥repo <1911140076s@ed.fuk.kindai.ac.jp>
2023-07-02 10:07:01 +0900

    ホーム画面の変更 * 毎月の抱負をトップ画面に記載

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'devlog';
+export const SITE_TITLE = 'Open devlog';
 export const SITE_DESCRIPTION = '開発系の記事を更新中！';
 
