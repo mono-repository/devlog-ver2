a6d6b2c9cbfef30b0adb28114619d2af633985fa
mio¥repo <1911140076s@ed.fuk.kindai.ac.jp>
2023-07-03 09:03:17 +0900

    前回上げ忘れの続き

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Open devlog';
-export const SITE_DESCRIPTION = '開発系の記事を更新中！';
+export const SITE_TITLE = 'open-Devlog';
+export const SITE_DESCRIPTION = 'archives of development';
 
