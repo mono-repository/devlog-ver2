fbba3e883f83da4a8bed943e798583f69fb17a0d
mio¥repo <1911140076s@ed.fuk.kindai.ac.jp>
2023-07-04 12:06:03 +0900

    変更点 * デジタル庁からのアイコン追加 →あんまり必要なさそうなので、削除してもOK * Header変更 などなど

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'open-Devlog';
-export const SITE_DESCRIPTION = 'archives of development';
+export const SITE_TITLE = 'open-devlog';
+export const SITE_DESCRIPTION = 'develop archive';
 
