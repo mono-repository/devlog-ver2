47f2f3a7bbbe32b05c6fc792bf700c8fc7248c91
mio¥repo <1911140076s@ed.fuk.kindai.ac.jp>
2023-07-05 09:34:27 +0900

    メニューのタイトル変更 card.astroの変更

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'open-devlog';
-export const SITE_DESCRIPTION = 'develop archive';
-
+export const SITE_DESCRIPTION = 'developper archive';
+export const AUTHOR = "Mio Yamamoto";
